73f77fbe351d49ed58b59eca46ffaf11435d7761
Teerapat Limpawittayakul <teerapat.l@arise.tech>
2024-04-22 12:04:15 +0700

    add easy calculator

--- app/src/main/java/com/teerapat/myapplication/MainActivity.kt
+++ app/src/main/java/com/teerapat/myapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.teerapat.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText1 = findViewById<EditText>(R.id.numberOneEt)
+        val editText2 = findViewById<EditText>(R.id.numberTwoEt)
+        val result = findViewById<TextView>(R.id.resultTv)
+        val button = findViewById<Button>(R.id.calculateButton)
+
+        button.setOnClickListener{
+            val num1 = editText1.text.toString().toIntOrNull() ?: 0
+            val num2 = editText2.text.toString().toIntOrNull() ?: 0
+            val sum = num1 + num2
+            result.text = sum.toString()
+        }
     }
 }
